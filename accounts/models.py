235ede1b0fc69d7a10197f74ebab95cca10a359f
Mehdi Jahani <80170150+mehdijahani1998@users.noreply.github.com>
2023-02-08 17:07:39 +0330

    added customized user and some models. It's incomplete

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,24 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    adress = models.TextField()
+    age = models.PositiveSmallIntegerField()
+    
+    description = models.TextField()
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    GENDER_CHOICES = (
+        ("M", "male"),
+        ("F", "female"),
+    )
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=1)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    phone = models.CharField(max_length=15)
+
--- charities/models.py
+++ charities/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+from accounts.models import *
 
 
 class Benefactor(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    EXP_CHOICE = [(0, 0), (1,1), (2,2)]
+    experience = models.SmallIntegerField(choices=EXP_CHOICE, default=0)
+    free_time_per_week = models.PositiveSmallIntegerField(default=0)
 
 
 class Charity(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    reg_number = models.CharField(max_length=10)
 
 
 class Task(models.Model):
-    pass
+    pass    
